99791f1576c5a24eacf5ecfab33f062849a9ab14
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-02-13 15:49:44 -0300

    adds right click functionality to tabs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { SpotifyMediaInfoHandler } from './SpotifyMediaInfoHandler';
 import { SpotifyCrawler } from './SpotifyCrawler';
 import { DeezerSearch } from './DeezerSearch';
 
-const BUTTON_ID = "open-in-deezer";
+const BUTTON_ID = "open-in-deezer-button";
+const TAB_ID = "open-in-deezer-tab";
 
 function registerContextMenuButton(deezer) {
     browser.contextMenus.create({
@@ -17,6 +18,18 @@ function registerContextMenuButton(deezer) {
         contexts: ["link"]
     });
 
+    browser.contextMenus.create({
+        id: TAB_ID,
+        title: "Open in Deezer",
+        documentUrlPatterns: [
+            'https://open.spotify.com/track/*',
+            'https://open.spotify.com/artist/*',
+            'https://open.spotify.com/album/*',
+            'https://open.spotify.com/playlist/*',
+        ],
+        contexts: ["tab"]
+    });
+
     function handleUrl(urlString) {
         new SpotifyCrawler()
             .fetchMediaInfo(urlString)
@@ -27,10 +40,12 @@ function registerContextMenuButton(deezer) {
     }
 
     browser.contextMenus.onClicked.addListener((info) => {
-        if (info.menuItemId !== BUTTON_ID)
+        console.log(info);
+        if ([BUTTON_ID, TAB_ID].indexOf(info.menuItemId) === -1)
             return;
 
-        handleUrl(info.linkUrl);
+        const urlString = info.linkUrl || info.pageUrl;
+        handleUrl(urlString);
     })
 }
 
